5a9c7faff0a77cdd7c797c40c3b0252efb7f8132
collegesenior <jameswick9080@gmail.com>
2026-03-27 21:55:53 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,33 +1,12 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 // Global type declaration at top level
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is missing from environment variables!");
-}
-
-const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false 
-  },
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-})
-
-const adapter = new PrismaPg(pool)
-
 const prismaClientSingleton = () => {
   return new PrismaClient({ 
-    adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
   })
 }
